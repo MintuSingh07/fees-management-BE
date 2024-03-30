6db23dd16363009b85c77c7c047ef3b645f0781b
Mintu Singh <mintusingh2006j@gmail.com>
2024-03-30 17:54:26 +0530

    backend v2.01

--- index.js
+++ index.js
@@ -19,8 +19,21 @@ mongoose.connect(process.env.MONGODB_URL)
     .then(() => console.log("DB connected"))
     .catch((err) => console.log(`Error is ${err}`));
 
+//! Middleware to verify student
+function verifyToken(req, res, next) {
+    const token = req.header('Authorization');
 
-//! ADD STUDENTS API
+    if (!token) return res.status(401).json({ error: 'Login first' });
+    try {
+        const decoded = jwt.decode(token, process.env.SECRET);
+        req.uuid = decoded.uuid;
+        next();
+    } catch (error) {
+        res.status(401).json({ error: 'Invalid token' });
+    }
+}
+
+//? ADD STUDENTS API
 app.post('/add-std', async (req, res) => {
     const { fullName, phone, stdClass } = req.body;
 
@@ -42,93 +55,7 @@ app.post('/add-std', async (req, res) => {
         res.status(400).json({ error: "Student Already Exists" });
     }
 });
-
-//! SHOW STUDENTS API
-app.get("/std-list", async (req, res) => {
-    const stdData = await Student.find({});
-    res.status(200).json(stdData);
-});
-
-//! STUDENT PAYMENT UPDATE API
-app.put('/update-payment/:uuid', async (req, res) => {
-    const { uuid } = req.params;
-    const { isPaid } = req.body;
-
-    try {
-        const student = await Student.findOneAndUpdate({ uuid }, { isPaid }, { new: true });
-
-        if (!student) {
-            return res.status(404).json({ message: "Student not found" });
-        }
-        res.json({ message: "Payment status updated successfully", student }); // remove student from json when deploy.
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-
-});
-
-//! SAVE CURRENT MONTH DATA
-// cron.schedule('0 23 7 * *', async () => {
-//     try {
-//         const students = await Student.find();
-//         const currentDate = new Date();
-//         const currentYear = currentDate.getFullYear();
-//         const currentMonth = currentDate.toLocaleString('default', { month: 'long' });
-
-//         // Collect all students data
-//         let feesData = await FeesData.findOne({ year: currentYear });
-//         if (!feesData) {
-//             feesData = new FeesData({ year: currentYear });
-//         }
-
-//         feesData[currentMonth] = students.map(student => ({ name: student.fullName, isPaid: student.isPaid, uuid: student.uuid }));
-
-//         // Save students data
-//         await feesData.save();
-//         console.log(`Date Stored`);
-//     }
-//     catch (error) {
-//         console.error('Error resetting payment status:', error);
-//     }
-// }, {
-//     scheduled: true,
-//     timezone: "Asia/Kolkata"
-// });
-
-// // RESET STUDENTS FEES DATA EVERY MONTH'S 8TH DAY
-// cron.schedule('0 0 8 * *', async () => {
-//     try {
-//         const students = await Student.find();
-//         // Reset payment status for all students
-//         for (const student of students) {
-//             student.isPaid = false;
-//             await student.save();
-//         }
-
-//         console.log(`Reset payment status for all students.`);
-//     } catch (error) {
-//         console.error('Error resetting payment status:', error);
-//     }
-// }, {
-//     scheduled: true,
-//     timezone: "Asia/Kolkata"
-// });
-
-//! Middleware to verify token
-function verifyToken(req, res, next) {
-    const token = req.header('Authorization');
-
-    if (!token) return res.status(401).json({ error: 'Login first' });
-    try {
-        const decoded = jwt.decode(token, process.env.SECRET);
-        req.uuid = decoded.uuid;
-        next();
-    } catch (error) {
-        res.status(401).json({ error: 'Invalid token' });
-    }
-}
-
-//! LOGIN
+//? LOGIN
 app.post('/login', async (req, res) => {
     const { fullName, uuid, stdClass } = req.body;
 
@@ -149,8 +76,12 @@ app.post('/login', async (req, res) => {
         res.status(400).json({ error: error });
     }
 });
-
-//! Profile
+//? SHOW STUDENTS API
+app.get("/std-list", async (req, res) => {
+    const stdData = await Student.find({});
+    res.status(200).json(stdData);
+});
+//? Profile
 app.get('/profile', verifyToken, async (req, res) => {
     const token = req.cookies.token;
 
@@ -166,7 +97,70 @@ app.get('/profile', verifyToken, async (req, res) => {
     } catch (error) {
         res.status(401).json({ message: "Invalid token" });
     }
-})
+});
+//? STUDENT PAYMENT UPDATE API
+app.put('/update-payment/:uuid', async (req, res) => {
+    const { uuid } = req.params;
+    const { isPaid } = req.body;
+
+    try {
+        const student = await Student.findOneAndUpdate({ uuid }, { isPaid }, { new: true });
+
+        if (!student) {
+            return res.status(404).json({ message: "Student not found" });
+        }
+        res.json({ message: "Payment status updated successfully", student }); // remove student from json when deploy.
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+
+});
+
+//? SAVE CURRENT MONTH DATA
+cron.schedule('0 23 7 * *', async () => {
+    try {
+        const students = await Student.find();
+        const currentDate = new Date();
+        const currentYear = currentDate.getFullYear();
+        const currentMonth = currentDate.toLocaleString('default', { month: 'long' });
+
+        // Collect all students data
+        let feesData = await FeesData.findOne({ year: currentYear });
+        if (!feesData) {
+            feesData = new FeesData({ year: currentYear });
+        }
+
+        feesData[currentMonth] = students.map(student => ({ name: student.fullName, isPaid: student.isPaid, uuid: student.uuid }));
+
+        // Save students data
+        await feesData.save();
+        console.log(`Date Stored`);
+    }
+    catch (error) {
+        console.error('Error resetting payment status:', error);
+    }
+}, {
+    scheduled: true,
+    timezone: "Asia/Kolkata"
+});
+//? RESET STUDENTS FEES DATA EVERY MONTH'S 8TH DAY
+cron.schedule('0 0 8 * *', async () => {
+    try {
+        const students = await Student.find();
+        // Reset payment status for all students
+        for (const student of students) {
+            student.isPaid = false;
+            await student.save();
+        }
+
+        console.log(`Reset payment status for all students.`);
+    } catch (error) {
+        console.error('Error resetting payment status:', error);
+    }
+}, {
+    scheduled: true,
+    timezone: "Asia/Kolkata"
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
